132e7d5086c5922f874466a21e55a7f23f77a277
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-26 02:27:00 -0300

    client e admin

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,1 +1,19 @@
-export default()=> <h1>Admin</h1>;
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function AdminDashboard() {
+
+  useEffect(() => {
+    async function check() {
+      const { data } = await supabase.auth.getUser();
+      if (data.user?.user_metadata.role !== "admin") {
+        window.location.href = "/login";
+      }
+    }
+    check();
+  }, []);
+
+  return (
+    <h1>Painel do Administrador</h1>
+  );
+}
--- src/pages/ClientDashboard.tsx
+++ src/pages/ClientDashboard.tsx
@@ -1,1 +1,19 @@
-export default()=> <h1>Client</h1>;
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function ClientDashboard() {
+
+  useEffect(() => {
+    async function check() {
+      const { data } = await supabase.auth.getUser();
+      if (data.user?.user_metadata.role !== "client") {
+        window.location.href = "/login";
+      }
+    }
+    check();
+  }, []);
+
+  return (
+    <h1>Painel do Cliente</h1>
+  );
+}
