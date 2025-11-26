dc87b23fcbaee576c82a4168178e14d562ffaba6
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-26 11:35:20 -0300

    tre

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-export default defineConfig({ plugins:[react()]})
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    host: true,
+  },
+});
