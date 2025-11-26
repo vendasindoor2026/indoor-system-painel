92cda0d32332b6e88ac27b23a7ad36d88a9b633d
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-26 01:30:15 -0300

    login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,27 @@
-import * as React from "react";
-import React from 'react';
-import {Link} from 'react-router-dom';
-export default()=> (<header className='p-4 shadow'>{
-<Link to='/'><img src='/assets/brand/logo.png' height='50'/>
-</Link>}<Link to='/login'>Login</Link></header>);
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="p-4 shadow flex items-center justify-between bg-white/10 backdrop-blur-md">
+      
+      {/* Logo pequena */}
+      <Link to="/">
+        <img
+          src="/assets/brand/logo.png"
+          alt="Logo"
+          className="h-12 w-auto"  // <<< Agora fica pequeno sempre
+        />
+      </Link>
+
+      {/* Botão Login discreto */}
+      <Link
+        to="/login"
+        className="text-white font-semibold hover:underline"
+      >
+        Login
+      </Link>
+
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
-import React from 'react';import {createRoot} from 'react-dom/client';import {BrowserRouter} from 'react-router-dom';import App from './App';import './styles/index.css';createRoot(document.getElementById('root')!).render(<BrowserRouter><App/></BrowserRouter>);
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
+createRoot(document.getElementById('root')!).render(<BrowserRouter><App/></BrowserRouter>);
