b4a9d4f7911fa22f58c388c09132f283c456f56b
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-26 01:17:56 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,6 @@
-import React from 'react';import {Link} from 'react-router-dom';export default()=> (<header className='p-4 shadow'>{<Link to='/'><img src='/assets/brand/logo.png' height='50'/></Link>}<Link to='/login'>Login</Link></header>);
+import * as React from "react";
+import React from 'react';
+import {Link} from 'react-router-dom';
+export default()=> (<header className='p-4 shadow'>{
+<Link to='/'><img src='/assets/brand/logo.png' height='50'/>
+</Link>}<Link to='/login'>Login</Link></header>);
